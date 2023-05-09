b94004a7fe0b10c00e7e0a845881a1af8a42c880
luizreboucas <primoneto@gmail.com>
2023-05-09 15:23:39 -0300

    commit final

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,18 @@
-import contatoRoutes from "./contatoRoutes.js"
-import express from "express"
-import cors from "cors"
+import contatoRoutes from './contatoRoutes.js'
+import express from 'express'
+import cors from 'cors'
 
 
 const routes = (app)=>{
-    app.get("/",(req,res)=>{
-        res.json({message: "servidor rodando"})
-    })
+	app.get('/',(req,res)=>{
+		res.json({message: 'servidor rodando'})
+	})
     
-    app.use(
-        cors(),
-        express.json(),  
-        contatoRoutes
-    )
+	app.use(
+		cors(),
+		express.json(),  
+		contatoRoutes
+	)
 }
 
 export default routes
